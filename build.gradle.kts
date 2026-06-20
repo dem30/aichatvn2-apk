3bffab63afcb60446f80c7bc46ab47596b82241c
dem30 <another.vin1@gmail.com>
2026-06-20 17:39:24 +0700

    hi

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application") version "8.4.2" apply false
-    
-   id("com.google.dagger.hilt.android") version "2.59.2" apply false
+    id("com.google.dagger.hilt.android") version "2.58" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
 id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
